26673512fa64b1d0c003c55d8e86c08c308d4d7b
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2025-08-16 02:19:11 +0530

    feat: integrate Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,24 +6,23 @@ import ReactQueryProvider from "@/lib/react-query-provider";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import { Suspense } from "react";
-import { SiteHeader } from "@/components/site-header";
 import { cn } from "@/lib/utils";
 import { PageTransition } from "@/components/page-transition";
-import { SiteFooter } from "@/components/site-footer";
 import { ReactFlowProvider } from "@xyflow/react";
+import { Analytics } from "@vercel/analytics/next";
 
 // Use only Google Fonts to avoid any local font references
 const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
   weight: ["300", "400", "500", "600"],
-})
+});
 
 const fontHeading = Montserrat({
   subsets: ["latin"],
   variable: "--font-heading",
   weight: ["600", "700"],
-})
+});
 
 export const metadata: Metadata = {
   title: "SemantiPay",
@@ -33,7 +32,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -41,28 +40,29 @@ export default function RootLayout({
         className={cn(
           "min-h-screen bg-background font-sans antialiased font-light",
           fontSans.variable,
-          fontHeading.variable,
+          fontHeading.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="relative flex min-h-screen flex-col">
-            
             <Suspense>
               <PageTransition>
                 <ReactQueryProvider>
                   <ReactFlowProvider>
-                <div className="flex-1">
-                  {children}
-                </div>
-                </ReactFlowProvider>
+                    <div className="flex-1">{children}</div>
+                  </ReactFlowProvider>
                 </ReactQueryProvider>
               </PageTransition>
             </Suspense>
-            
           </div>
         </ThemeProvider>
         <Toaster position="top-center" />
       </body>
     </html>
-  )
+  );
 }
